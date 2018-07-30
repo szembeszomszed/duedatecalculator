605caba289d9e829d5bd3f57780bbab0428a4c9e
rmate <retsagimate@gmail.com>
2018-07-30 11:50:09 +0200

    Restructure DateTimeConstants

--- DueDateCalculator/src/main/java/hu/retsagimate/duedatecalculator/DateTimeConstants.java
+++ DueDateCalculator/src/main/java/hu/retsagimate/duedatecalculator/DateTimeConstants.java
@@ -6,9 +6,6 @@ package hu.retsagimate.duedatecalculator;
  */
 public class DateTimeConstants {
     
-    private DateTimeConstants() {        
-    }
-    
     public static final String DATE_FORMAT = "dd-MM-yyyy KK:mma";
     
     public static final int WORKING_DAY_START_HOUR = 9;
@@ -21,5 +18,8 @@ public class DateTimeConstants {
     
     public static final int NUMBER_OF_NON_WORKING_HOURS_PER_DAY = 24 - NUMBER_OF_WORKING_HOURS_PER_DAY;
     
-    public static final long ONE_HOUR_IN_MILLISECONDS = 3600 * 1000;    
+    public static final long ONE_HOUR_IN_MILLISECONDS = 3600 * 1000;
+    
+    private DateTimeConstants() {        
+    }    
 }
